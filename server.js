91c3f8322b41750f64648f60452947fc22541397
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2024-02-02 10:26:18 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/ping', (req, res) => {
-    res.send("pong")
+    res.json({'ping': "pong"})
 })
 app.listen(3000, () => {
     console.log("Server is running at port 3000")
