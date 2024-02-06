bd58cce3c1955f854e49347406e72bf9bdeee76e
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2024-02-06 11:01:36 +0530

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.get('/ping', (req, res) => {
-    res.json({'ping': "pong"})
+const mongoURI = 'mongodb+srv://pet_user_01:cutepets123@pets.dsug72y.mongodb.net/CutestPets?retryWrites=true&w=majority'
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}) 
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+db.once('open', () => {
+    console.log("Connected to MongoDB using Mongoose")
+})
+
+app.get('/', (req, res) => {
+    if(db.readyState === 1){
+        res.send("Connected to MongoDB successfully")
+    }else{
+        res.send("Failed to connect to MongoDB")
+    }
 })
 app.listen(3000, () => {
     console.log("Server is running at port 3000")
