fb9534db8461b89c1b4a34219db920c47f1372b2
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2024-02-08 10:04:32 +0530

    Created React App

---
+++ client/asap-cute-pets/src/components/Pets.jsx
@@ -0,0 +1,9 @@
+const Pets = () => {
+    return(
+        <div>
+            <h1>Welcome to Adorable Pet Breeds!</h1>
+            <p>Discover the most adorable pet breeds that will melt your heart.</p>
+        </div>
+    )
+}
+export default Pets;
