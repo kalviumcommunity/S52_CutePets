cf7db6825522458f871a587f9d7ea326ceae9b0e
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2024-02-20 18:02:24 +0530

    Added login, logout, signup

--- client/asap-cute-pets/src/App.jsx
+++ client/asap-cute-pets/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import CreatePet from './components/CreatePet'
 import UpdatePet from './components/UpdatePet'
 import Pets from './components/Pets'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path='/' element={<Pets />} />
           <Route path='/createPet' element={<CreatePet />} />
           <Route path='/updatePet/:id' element={<UpdatePet />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
